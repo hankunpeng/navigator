3b3ceb3ba1693e72b74b53ec6e68ee48f72d2baf
William Han <hankunpeng@gmail.com>
2019-03-14 15:43:25 +0800

    add location feature

--- app/src/main/java/han/kunpeng/navigator/MainActivity.java
+++ app/src/main/java/han/kunpeng/navigator/MainActivity.java
@@ -1,21 +1,39 @@
 package han.kunpeng.navigator;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.amap.api.maps.AMap;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.MyLocationStyle;
 
-public class MainActivity extends AppCompatActivity {
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AMap.OnMyLocationChangeListener {
+    private static final String TAG = "amap";
     private MapView mMapView;
     private AMap mAMap;
+    private MyLocationStyle mMyLocationStyle;
 
-    TabLayout mTabLayout;
-    TabLayout.Tab mTabFood;
-    TabLayout.Tab mTabCarPark;
-    TabLayout.Tab mTabGasStation;
+    private TabLayout mTabLayout;
+    private TabLayout.Tab mTabFood;
+    private TabLayout.Tab mTabCarPark;
+    private TabLayout.Tab mTabGasStation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
     private void init() {
         if (mAMap == null) {
             mAMap = mMapView.getMap();
+            setUpMap();
         }
 
         mTabLayout = findViewById(R.id.tab_layout);
@@ -66,12 +85,44 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+    /**
+     * 设置一些 AMap 的属性
+     */
+    private void setUpMap() {
+        // 设置默认定位状态
+        mMyLocationStyle = new MyLocationStyle().myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATE);
+        mAMap.setMyLocationStyle(mMyLocationStyle);
+
+        // 设置默认定位按钮是否显示
+        mAMap.getUiSettings().setMyLocationButtonEnabled(true);
+
+        // 默认为 false 表示隐藏定位层并不可触发定位
+        // 设置为 true 表示显示定位层并可触发定位
+        mAMap.setMyLocationEnabled(true);
+
+        // 设置 SDK 自带定位消息监听
+        mAMap.setOnMyLocationChangeListener(this);
+    }
+
     /**
      * 方法必须重写
      */
     @Override
     protected void onResume() {
         super.onResume();
+
+        try {
+            super.onResume();
+            if (Build.VERSION.SDK_INT >= 23) {
+                if (isNeedCheck) {
+                    checkPermissions(needPermissions);
+                }
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
         mMapView.onResume();
     }
 
@@ -100,5 +151,223 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         mMapView.onDestroy();
+    }
+
+    @Override
+    public void onMyLocationChange(Location location) {
+        // 定位回调监听
+        if (location != null) {
+            Log.e(TAG, "onMyLocationChange 定位成功， lat: " + location.getLatitude() + " lon: " + location.getLongitude());
+            Bundle bundle = location.getExtras();
+            if (bundle != null) {
+                int errorCode = bundle.getInt(MyLocationStyle.ERROR_CODE);
+                String errorInfo = bundle.getString(MyLocationStyle.ERROR_INFO);
+                // 定位类型，可能为 GPS、WIFI 等，具体可以参考官网的定位 SDK 介绍。
+                int locationType = bundle.getInt(MyLocationStyle.LOCATION_TYPE);
+
+                /*
+                errorCode
+                errorInfo
+                locationType
+                */
+                Log.e(TAG, "定位信息， code: " + errorCode + " errorInfo: " + errorInfo + " locationType: " + locationType);
+            } else {
+                Log.e(TAG, "定位信息， bundle is null ");
+
+            }
+
+        } else {
+            Log.e(TAG, "定位失败");
+        }
+    }
+
+
+    /*************************************** 权限检查******************************************************/
+
+    /**
+     * 需要进行检测的权限数组
+     */
+    protected String[] needPermissions = {
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.READ_PHONE_STATE
+    };
+
+    private static final int PERMISSON_REQUESTCODE = 0;
+
+    /**
+     * 判断是否需要检测，防止不停的弹框
+     */
+    private boolean isNeedCheck = true;
+
+    /**
+     * @param
+     * @since 2.5.0
+     */
+    @TargetApi(23)
+    private void checkPermissions(String... permissions) {
+        try {
+            if (Build.VERSION.SDK_INT >= 23 && getApplicationInfo().targetSdkVersion >= 23) {
+                List<String> needRequestPermissonList = findDeniedPermissions(permissions);
+                if (null != needRequestPermissonList
+                        && needRequestPermissonList.size() > 0) {
+                    try {
+                        String[] array = needRequestPermissonList.toArray(new String[needRequestPermissonList.size()]);
+                        Method method = getClass().getMethod("requestPermissions", new Class[]{String[].class, int.class});
+                        method.invoke(this, array, 0);
+                    } catch (Throwable e) {
+
+                    }
+                }
+            }
+
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 获取权限集中需要申请权限的列表
+     *
+     * @param permissions
+     * @return
+     * @since 2.5.0
+     */
+    @TargetApi(23)
+    private List<String> findDeniedPermissions(String[] permissions) {
+        try {
+            List<String> needRequestPermissonList = new ArrayList<String>();
+            if (Build.VERSION.SDK_INT >= 23 && getApplicationInfo().targetSdkVersion >= 23) {
+                for (String perm : permissions) {
+                    if (checkMySelfPermission(perm) != PackageManager.PERMISSION_GRANTED
+                            || shouldShowMyRequestPermissionRationale(perm)) {
+                        needRequestPermissonList.add(perm);
+                    }
+                }
+            }
+            return needRequestPermissonList;
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private int checkMySelfPermission(String perm) {
+        try {
+            Method method = getClass().getMethod("checkSelfPermission", new Class[]{String.class});
+            Integer permissionInt = (Integer) method.invoke(this, perm);
+            return permissionInt;
+        } catch (Throwable e) {
+        }
+        return -1;
+    }
+
+    private boolean shouldShowMyRequestPermissionRationale(String perm) {
+        try {
+            Method method = getClass().getMethod("shouldShowRequestPermissionRationale", new Class[]{String.class});
+            Boolean permissionInt = (Boolean) method.invoke(this, perm);
+            return permissionInt;
+        } catch (Throwable e) {
+        }
+        return false;
+    }
+
+    /**
+     * 检测是否说有的权限都已经授权
+     *
+     * @param grantResults
+     * @return
+     * @since 2.5.0
+     */
+    private boolean verifyPermissions(int[] grantResults) {
+        try {
+            for (int result : grantResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    return false;
+                }
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
+
+    @TargetApi(23)
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] paramArrayOfInt) {
+        try {
+            if (Build.VERSION.SDK_INT >= 23) {
+                if (requestCode == PERMISSON_REQUESTCODE) {
+                    if (!verifyPermissions(paramArrayOfInt)) {
+                        showMissingPermissionDialog();
+                        isNeedCheck = false;
+                    }
+                }
+            }
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 显示提示信息
+     *
+     * @since 2.5.0
+     */
+    private void showMissingPermissionDialog() {
+        try {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("提示");
+            builder.setMessage("当前应用缺少必要权限。\\n\\n请点击\\\"设置\\\"-\\\"权限\\\"-打开所需权限");
+
+            // 拒绝, 退出应用
+            builder.setNegativeButton("取消",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            try {
+                                finish();
+                            } catch (Throwable e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+
+            builder.setPositiveButton("设置",
+                    new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            try {
+                                startAppSettings();
+                            } catch (Throwable e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+
+            builder.setCancelable(false);
+
+            builder.show();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 启动应用的设置
+     *
+     * @since 2.5.0
+     */
+    private void startAppSettings() {
+        try {
+            Intent intent = new Intent(
+                    Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            intent.setData(Uri.parse("package:" + getPackageName()));
+            startActivity(intent);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
     }
 }
